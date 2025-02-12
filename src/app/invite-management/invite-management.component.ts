2715e3ecf4b59dff41d43c04538a31ea42868eee
LmaonadeFTW <kubon79@gmail.com>
2025-02-12 23:34:36 +0800

    added mat-stepper to invite-management

--- src/app/invite-management/invite-management.component.ts
+++ src/app/invite-management/invite-management.component.ts
@@ -8,6 +8,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
 @Component({
   selector: 'app-invite-management.component',
   imports: [
@@ -16,6 +21,13 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatListModule,
     MatIconModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
   ],
   templateUrl: './invite-management.component.html',
   styleUrl: './invite-management.component.scss',
@@ -27,4 +39,17 @@ export class InviteManagementComponentComponent {
   }
 
   invite$: Observable<InviteWithUsers> | undefined;
+  private _formBuilder = inject(FormBuilder);
+  readonly bestBoys: string[] = ['Samoyed', 'Akita Inu', 'Alaskan Malamute', 'Siberian Husky'];
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+
+
+
 }
