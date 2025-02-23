248cc58a380796a40c98005eef9c7e665b3fe9db
Darren Meiring <dpmeiring@gmail.com>
2025-02-23 21:24:25 +0800

    Made invite login more intelligent

--- src/app/invite-signin/invite-signin.component.ts
+++ src/app/invite-signin/invite-signin.component.ts
@@ -1,6 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Observable,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
 
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,6 +18,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { Invite } from '../../types';
+import { ApiService } from '../api.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-invite-signin.component',
@@ -19,18 +31,42 @@ import { Invite } from '../../types';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './invite-signin.component.html',
   styleUrl: './invite-signin.component.scss',
 })
 export class InviteSigninComponentComponent {
   private router = inject(Router);
+  private api = inject(ApiService);
+
   codeControl = new FormControl<string>('', { nonNullable: true });
-  invite$: Observable<Invite> | undefined;
+  loading: boolean = false;
+  invite: Invite | null = null;
+
+  ngOnInit() {}
 
   routeToInvite() {
-    console.log('Navigating to invite...');
-    localStorage.setItem('inviteCode', this.codeControl.value);
-    this.router.navigate(['/invite', this.codeControl.value]);
+    this.loading = true;
+    this.api
+      .getInviteByCode(this.codeControl.value)
+      .pipe(
+        catchError((err) => {
+          console.error(err);
+          this.loading = false;
+          this.codeControl.setErrors({ invalidCode: true });
+          return of(null);
+        })
+      )
+      .subscribe((invite) => {
+        console.log(invite);
+        if (invite) {
+          this.invite = invite;
+          this.loading = false;
+          this.router.navigate(['/invite', this.codeControl.value]);
+          localStorage.setItem('inviteCode', this.codeControl.value);
+        }
+      });
   }
 }
