c624a8fd17998b5625540908f6955fde6abe3229
LmaonadeFTW <kubon79@gmail.com>
2025-05-27 17:54:40 +0800

    updated coordinates

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
   swingsLatLong: {
-    lat: -33.6884474,
-    lng: 115.0262766,
+    lat: -33.68844903956983,
+    lng: 115.02885291097918,
   },
   weddingDate: new Date('2025-11-22'),
   weddingAddress: '2807 Caves Rd, Yallingup WA 6282',
