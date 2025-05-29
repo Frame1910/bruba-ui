8684a974c20708c6e982620c562be4f9cca40239
LmaonadeFTW <kubon79@gmail.com>
2025-05-29 23:24:51 +0800

    styling and formatting

--- src/app/main-screen/accommodation/accommodation.component.ts
+++ src/app/main-screen/accommodation/accommodation.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-accommodation',
   imports: [
     MatCardModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatDividerModule
   ],
   templateUrl: './accommodation.component.html',
   styleUrl: './accommodation.component.scss'
