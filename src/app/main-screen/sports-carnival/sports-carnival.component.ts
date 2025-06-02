3ef93bbac350a6a644db87630e19ac04bec1668d
LmaonadeFTW <kubon79@gmail.com>
2025-06-02 23:25:41 +0800

    Sports carnival initial implementation

--- src/app/main-screen/sports-carnival/sports-carnival.component.ts
+++ src/app/main-screen/sports-carnival/sports-carnival.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-sports-carnival',
   imports: [
     MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatMenuModule
   ],
   templateUrl: './sports-carnival.component.html',
   styleUrl: './sports-carnival.component.scss'
 })
 export class SportsCarnivalComponent {
+  sportsCarnivalGoogleCalendarURL: string = environment.sportsCarnivalGoogleCalendarURL;
+  sportsCarnivalOutlookCalendarURL: string = environment.sportsCarnivalOutlookCalendarURL;
 
 }
