1c8e8294e62e99fc5b48f7d3572ae6a7b1f47c31
Darren Meiring <dpmeiring@gmail.com>
2025-06-08 20:03:39 +0800

    Added tab index pre-selection

--- src/app/main-screen/main-screen.component.ts
+++ src/app/main-screen/main-screen.component.ts
@@ -13,6 +13,8 @@ import { ApiService } from '../api.service';
 import { AccommodationComponent } from './accommodation/accommodation.component';
 import { LocationComponent } from './location/location.component';
 import { SportsCarnivalComponent } from './sports-carnival/sports-carnival.component';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-main-screen',
@@ -35,14 +37,23 @@ import { SportsCarnivalComponent } from './sports-carnival/sports-carnival.compo
 })
 export class MainScreenComponent {
   private api = inject(ApiService);
+  private activatedRoute = inject(ActivatedRoute);
   invite: InviteWithUsers | undefined;
+  currentTab: number = 0;
 
   ngOnInit() {
     const code = localStorage.getItem('inviteCode');
-    this.api.getInvitees(code!).subscribe((invite) => {
-      console.log('Got invite data!');
-      this.invite = invite;
-      console.log(this.invite)
-    });
+    this.api
+      .getInvitees(code!)
+      .pipe(
+        switchMap((invite) => {
+          this.invite = invite;
+          return this.activatedRoute.queryParamMap;
+        })
+      )
+      .subscribe((params) => {
+        this.currentTab = parseInt(params.get('tabIndex') || '0', 10);
+        console.log('Current tab index:', this.currentTab);
+      });
   }
 }
