419b3534b8057f4f370db545ab1f6d84dfe701b2
LmaonadeFTW <kubon79@gmail.com>
2025-06-18 22:02:48 +0800

    updated declined screen to use localstorage names

--- src/app/sad-screen/sad-screen.component.ts
+++ src/app/sad-screen/sad-screen.component.ts
@@ -1,18 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sad-screen',
-  imports: [
-    MatButtonModule,
-    MatIconModule
-  ],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './sad-screen.component.html',
-  styleUrl: './sad-screen.component.scss'
+  styleUrl: './sad-screen.component.scss',
 })
 export class SadScreenComponent {
   readonly router = inject(Router);
 
+  renderName() {
+    const groomName = localStorage.getItem('groomName') || 'Jakub';
+    const nameOrder = localStorage.getItem('nameOrder') || 'BJ';
+    if (nameOrder === 'JB') {
+      return `Brooke & ${groomName}`;
+    } else {
+      return `${groomName} & Brooke`;
+    }
+  }
 }
