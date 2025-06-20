5de3b04673f2d56a18582d73c5d5c9e232c2a2cf
LmaonadeFTW <kubon79@gmail.com>
2025-06-20 23:21:02 +0800

    return button

--- src/app/rice-cooker/rice-cooker.component.ts
+++ src/app/rice-cooker/rice-cooker.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-rice-cooker',
-  imports: [],
+  imports: [MatIconModule, RouterModule, MatButtonModule],
   templateUrl: './rice-cooker.component.html',
-  styleUrl: './rice-cooker.component.scss'
+  styleUrl: './rice-cooker.component.scss',
 })
-export class RiceCookerComponent {
-
-}
+export class RiceCookerComponent {}
