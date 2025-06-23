0fcf29d94a500d077bc2b8fa9c61318a7062f341
LmaonadeFTW <kubon79@gmail.com>
2025-06-23 22:42:55 +0800

    updated sports carnival time to 2pm-7pm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
   outlookCalendarURL:
     'https://outlook.live.com/calendar/0/action/compose?allday=false&body=bruba.wedding&enddt=2025-11-23T00%3A00%3A00&location=2807%20Caves%20Rd%2C%20Yallingup%20WA%206282&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2025-11-22T15%3A30%3A00&subject=Wedding%20of%20Brooke%20%26%20Jakub',
   sportsCarnivalGoogleCalendarURL:
-    'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20251119T080000Z%2F20251119T130000Z&details=bruba.wedding&location=23%20Palladio%20Pass%2C%20Clarkson%20WA%206030&text=Sports%20Carnival%21',
+    'https://calendar.google.com/calendar/u/0/r/eventedit?dates=20251119T060000Z/20251119T110000Z&details=bruba.wedding&location=23+Palladio+Pass,+Clarkson+WA+6030&text=Sports+Carnival!',
   sportsCarnivalOutlookCalendarURL:
-    'https://outlook.live.com/calendar/0/action/compose?allday=false&body=bruba.wedding&enddt=2025-11-19T21%3A00%3A00&location=23%20Palladio%20Pass%2C%20Clarkson%20WA%206030&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2025-11-19T16%3A00%3A00&subject=Sports%20Carnival%21',
+    'https://outlook.live.com/calendar/0/action/compose?allday=false&body=bruba.wedding&enddt=2025-11-19T19%3A00%3A00&location=23%20Palladio%20Pass%2C%20Clarkson%20WA%206030&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2025-11-19T14%3A00%3A00&subject=Sports%20Carnival%21',
 };
