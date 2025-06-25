97e53f15eca9834e8aac16d837e75ec095db4ad8
LmaonadeFTW <kubon79@gmail.com>
2025-06-25 20:36:22 +0800

    updated calendar invites in dev env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -8,12 +8,12 @@ export const environment = {
   weddingDate: new Date('2025-11-22'),
   weddingAddress: '2807 Caves Rd, Yallingup WA 6282',
   weddingVenueName: 'Swings & Roundabouts',
-  googleCalendarURL:
+ googleCalendarURL:
     'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20251122T073000Z%2F20251122T160000Z&details=bruba.wedding&location=2807%20Caves%20Rd%2C%20Yallingup%20WA%206282&text=Wedding%20of%20Brooke%20%26%20Jakub',
   outlookCalendarURL:
     'https://outlook.live.com/calendar/0/action/compose?allday=false&body=bruba.wedding&enddt=2025-11-23T00%3A00%3A00&location=2807%20Caves%20Rd%2C%20Yallingup%20WA%206282&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2025-11-22T15%3A30%3A00&subject=Wedding%20of%20Brooke%20%26%20Jakub',
   sportsCarnivalGoogleCalendarURL:
-    'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20251119T080000Z%2F20251119T130000Z&details=bruba.wedding&location=23%20Palladio%20Pass%2C%20Clarkson%20WA%206030&text=Sports%20Carnival%21',
+    'https://calendar.google.com/calendar/u/0/r/eventedit?dates=20251119T060000Z/20251119T110000Z&details=bruba.wedding&location=23+Palladio+Pass,+Clarkson+WA+6030&text=Sports+Carnival!',
   sportsCarnivalOutlookCalendarURL:
-    'https://outlook.live.com/calendar/0/action/compose?allday=false&body=bruba.wedding&enddt=2025-11-19T21%3A00%3A00&location=23%20Palladio%20Pass%2C%20Clarkson%20WA%206030&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2025-11-19T16%3A00%3A00&subject=Sports%20Carnival%21',
+    'https://outlook.live.com/calendar/0/action/compose?allday=false&body=bruba.wedding&enddt=2025-11-19T19%3A00%3A00&location=23%20Palladio%20Pass%2C%20Clarkson%20WA%206030&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=2025-11-19T14%3A00%3A00&subject=Sports%20Carnival%21',
 };
