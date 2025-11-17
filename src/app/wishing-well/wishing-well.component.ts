8cf7313b15a2e54f50568b1eeea9dfbbe7853a65
Kuba <kubon79@gmail.com>
2025-11-17 13:21:23 +0800

    initial commit with route

---
+++ src/app/wishing-well/wishing-well.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wishing-well',
+  imports: [],
+  templateUrl: './wishing-well.component.html',
+  styleUrl: './wishing-well.component.scss'
+})
+export class WishingWellComponent {
+
+}
